372ace0731ff9e5c06f77bbe1de60433072f6b9a
nirs <nirs@reshet.tv>
2018-01-29 17:23:51 +0200

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import Layers from './containers/layers';
-import Legend from './containers/legend';
+import Character from './components/character';
 import './App.css';
 import {push} from 'react-router-redux'
 import {bindActionCreators} from 'redux'
@@ -26,57 +24,41 @@ const mapStateToProps = state => ({
 const App = (props) => (
 
     <div className="App">
-
-        <main>
-            <Layers />
-            <Legend />
-        </main>
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">{props.count} {props.opacityAmount}</h1>
-            <h1 className="App-title">{props.opacityAmount}</h1>
-        </header>
-
+        <div className="container">
+            <div className="content">
+                <ul className="legend">
+                    <li className="legend-item"></li>
+                    <li className="legend-item"><span>ראשון</span></li>
+                    <li className="legend-item"><span>שני</span></li>
+                    <li className="legend-item"><span>שלישי</span></li>
+                    <li className="legend-item"><span>רביעי</span></li>
+                    <li className="legend-item"><span>חמישי</span></li>
+                </ul>
+                <ul className="early">
+                    <li className="early-item"><span>מוקדם</span></li>
+                    <li className="early-item"><Character /></li>
+                    <li className="early-item"><Character /></li>
+                    <li className="early-item"><Character /></li>
+                    <li className="early-item"><Character /></li>
+                    <li className="early-item"><Character /></li>
+                </ul>
+                <ul className="late">
+                    <li className="late-item"><span>מאוחר</span></li>
+                    <li className="late-item"><Character /></li>
+                    <li className="late-item"><Character /></li>
+                    <li className="late-item"><Character /></li>
+                    <li className="late-item"><Character /></li>
+                    <li className="late-item"><Character /></li>
+                </ul>
+             </div>
+         </div>
         <div id="content">
-{/*            <div className="item one">
-                <p className=""><span>1</span></p>
-                <p className=""><span>2</span></p>
-                <p className=""><span>3</span></p>
-                <p className=""><span>4</span></p>
-            </div>*/}
             <div className="item two">
                 <button id="plus" onClick={(event) => { props.add_layer();props.increment();}}>+</button>
             </div>
-
             <div className="item three">
                 <button id="minus" onClick={(event) => { props.delete_layer((props.layers[props.layers.length-1].id));props.decrement();}}>-</button>
             </div>
-            {/*
-            <div className="item four">
-                <p>1</p><p>2</p><p>3</p>
-            </div>
-            <div className="item five">
-                <datalist id="tickmarks">
-                    <option value="0" label="0%"/>
-                    <option value="10"/>
-                    <option value="20"/>
-                    <option value="30"/>
-                    <option value="40"/>
-                    <option value="50" label="50%"/>
-                    <option value="60"/>
-                    <option value="70"/>
-                    <option value="80"/>
-                    <option value="90"/>
-                    <option value="100" label="100%"/>
-                </datalist>
-                <input type="range" min="0" max="10" step="0.01" list="tickmarks"
-                       onChange={props.change_range_amt}
-                />
-            </div>
-            <div className="item six"></div>
-            <div className="item seven"></div>
-            <div className="item eight"></div>
-            <div className="item nine"></div>*/}
         </div>
     </div>
 );
